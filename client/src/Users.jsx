bd5c5c831d7165ed2ce98aba622520b431efe670
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2024-11-02 13:22:28 +0500

    add .

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -5,13 +5,13 @@ function Users () {
     const [users,setUsers] =useState([])
 
   useEffect(()=>{
-         axios.get('https://crud-app-psi-rose.vercel.app')  //localhost:3001
+         axios.get('http://localhost:3001')  //localhost:3001
          .then(result => setUsers(result.data))
 .catch(err =>console.log(err) )
   },[])
 
   const handleDelete =(id) =>{
-    axios.delete('https://crud-app-psi-rose.vercel.app/deleteUser/'+id)      //localhost:3001
+    axios.delete('http://localhost:3001/deleteUser/'+id)      //localhost:3001
    
     .then(res=> {console.log(res)
       window.location.reload()
