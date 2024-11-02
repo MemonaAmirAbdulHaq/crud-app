0f78f79558f85fa7ee3eedbd0fa8466b4a318e1e
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2024-11-02 13:11:46 +0500

    add .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,19 +5,19 @@ import UpdateUser from './UpdateUser'
 
 
 import './App.css'
-import {Router,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
-    <Router>
+    <BrowserRouter>
     <Routes>
       <Route path='/' element={<Users/>}></Route>
       <Route path='/create' element={<CreateUser/>}></Route>
       <Route path='/update/:id' element={<UpdateUser />}></Route>
-      </Routes></Router>
+      </Routes></BrowserRouter>
    </div>
   )
 }
